7d185ddf2bb7071910f9bab0035a6293af58e244
Sanket Dasgupta <sanketdasgupta@gmail.com>
2020-04-10 23:40:56 +0530

    Add basic layout to front page

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#40407a`,
       marginBottom: `1.45rem`,
     }}
   >
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,18 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Hero from "../texts/Hero"
+import { Global } from "@emotion/core"
+import { Styled, Container, css } from "theme-ui"
+import Header from "../components/header"
 
 const IndexPage = () => (
-  <>
-    <SEO title="Home" />
-    <Hero />
-  </>
+  <Styled.root data-testid="theme-root">
+      <SEO title="Home" />
+      <Header siteTitle="SanketDG" />
+      <Layout>
+        <Hero />
+      </Layout>
+  </Styled.root>
 )
 
 export default IndexPage
