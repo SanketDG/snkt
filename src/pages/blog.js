5daf3a7593aebb2aec44130129c0d528018b0e7c
Sanket Dasgupta <sanketdasgupta@gmail.com>
2020-05-15 21:16:58 +0530

    Give shape to /blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,72 +1,84 @@
-// import React from "react"
-// import { Link, graphql } from "gatsby"
+import React from "react"
+import { Link, graphql } from "gatsby"
 
-// // import Bio from "../components/bio"
-// import Layout from "../components/layout"
-// import SEO from "../components/seo"
-// import { rhythm } from "../utils/typography"
+// import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
 
-// const BlogIndex = ({ data, location }) => {
-//     const siteTitle = data.site.siteMetadata.title
-//     const posts = data.allMarkdownRemark.edges
+const BlogIndex = ({ data, location }) => {
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
 
-//     return (
-//         <Layout location={location} title={siteTitle}>
-//             <SEO title="All posts" />
-//             {/* <Bio /> */}
-//             {posts.map(({ node }) => {
-//                 const title = node.frontmatter.title || node.fields.slug
-//                 return (
-//                     <article key={node.fields.slug}>
-//                         <header>
-//                             <h3
-//                                 style={{
-//                                     marginBottom: rhythm(1 / 4),
-//                                 }}
-//                             >
-//                                 <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-//                                     {title}
-//                                 </Link>
-//                             </h3>
-//                             <small>{node.frontmatter.date}</small>
-//                         </header>
-//                         <section>
-//                             <p
-//                                 dangerouslySetInnerHTML={{
-//                                     __html: node.frontmatter.description || node.excerpt,
-//                                 }}
-//                             />
-//                         </section>
-//                     </article>
-//                 )
-//             })}
-//         </Layout>
-//     )
-// }
+    return (
+        <div style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `2rem 1.0875rem`,
+            width: "80%"
+        }}>
+            {/* <Bio /> */}
+            {posts.map(({ node }) => {
+                const title = node.frontmatter.title || node.fields.slug
+                return (
+                    <div
+                      style={{
+                        paddingBottom: "0.5rem",
+                      }}
+                    >
+                      <p
+                        style={{
+                          fontSize: "40px",
+                          fontWeight: "500",
+                          marginBottom: "5px",
+                        }}
+                      >
+                        {title}
+                      </p>
+                      <p
+                        style={{
+                          fontSize: "20px",
+                          fontWeight: "100"
+                        }}
+                      >
+                        {node.frontmatter.date}
+                      </p>
+                      <section>
+                      <p
+                        dangerouslySetInnerHTML={{
+                          __html: node.frontmatter.description || node.excerpt,
+                        }}
+                      />
+                    </section>
+                    </div>
 
-// export default BlogIndex
+                )
+            })}
+        </div>
+    )
+}
 
-// export const pageQuery = graphql`
-//   query {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-//       edges {
-//         node {
-//           excerpt
-//           fields {
-//             slug
-//           }
-//           frontmatter {
-//             date(formatString: "MMMM DD, YYYY")
-//             title
-//             description
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          frontmatter {
+            path
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
