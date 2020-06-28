86878a1bdc692b073c0d8a3187b2c75d199e9fe7
Sanket Dasgupta <sanketdasgupta@gmail.com>
2020-06-28 23:18:52 +0530

    Add resume as page

---
+++ src/pages/resume.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Header from "../components/header"
+
+const Page = ({ data, location }) => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default Page
