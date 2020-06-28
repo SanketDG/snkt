c44d42db49390a0f1dbe2720ff3566f069d451f0
Sanket Dasgupta <sanketdasgupta@gmail.com>
2020-06-28 23:18:32 +0530

    Add Projects as markdown

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,12 +1,24 @@
 import React from "react"
 
 import Header from "../components/header"
+import Projects from "../texts/Projects"
 
 const BlogIndex = ({ data, location }) => {
 
   return (
     <>
       <Header />
+      <div
+        className="blog-main"
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1rem 1.0875rem`,
+          width: "80%",
+        }}
+      >
+        <Projects />
+      </div>
     </>
   )
 }
