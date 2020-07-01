c01da966b7b98ddd1a4a8888716ff5a816c04105
Sanket Dasgupta <sanketdasgupta@gmail.com>
2020-07-01 22:45:38 +0530

    Add basic cards for projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,8 +2,10 @@ import React from "react"
 
 import Header from "../components/header"
 import Projects from "../texts/Projects"
+import { Grid, Box, Card, Text } from "theme-ui"
 
 const BlogIndex = ({ data, location }) => {
+  let projects = ["portfolio", "awsomsauce", "modern-django-tooling", "khol"]
 
   return (
     <>
@@ -17,6 +19,37 @@ const BlogIndex = ({ data, location }) => {
           width: "80%",
         }}
       >
+        <h1>Projects</h1>
+        <h2>Ongoing</h2>
+        <Grid
+          gap={3}
+          columns={[2]}
+          sx={{
+            margin: "40px 0",
+          }}
+        >
+          {projects.map(project => {
+            return (
+              <Card
+                sx={{
+                  width: "100%",
+                  margin: "0 auto",
+                  border: "3px solid #000",
+                  padding: "15px",
+                  maxWidth: 400,
+                }}
+              >
+                <Text
+                  sx={{
+                    textAlign: "center",
+                  }}
+                >
+                  {project}
+                </Text>
+              </Card>
+            )
+          })}
+        </Grid>
         <Projects />
       </div>
     </>
